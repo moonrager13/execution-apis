ecbb75918e229156f523db7bda0ec78203450fb1
moonrager13 <moonrager13@gmail.com>
2026-07-09 23:45:35 -0500

    Add withdrawal security tests scaffold

---
+++ test/AgentExecutor.security.test.js
@@ -0,0 +1,7 @@
+const { expect } = require("chai");
+
+describe("AgentExecutor security", function () {
+  it("should define withdrawal protection tests", async function () {
+    expect(true).to.equal(true);
+  });
+});
