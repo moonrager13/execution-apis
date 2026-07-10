7197aa9f302669825958a41e4f494290d3b9cf7f
moonrager13 <moonrager13@gmail.com>
2026-07-09 22:41:31 -0500

    Lock AgentExecutor owner to fixed address

--- contracts/AgentExecutor.sol
+++ contracts/AgentExecutor.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract AgentExecutor is Ownable {
+    address public constant FIXED_OWNER = 0xfd1610f5eae31dd757e55d6b4ba543b80a2720b3;
+
     event Executed(address indexed target, uint256 value, bytes data);
 
-    constructor() Ownable(msg.sender) {}
+    constructor() Ownable(FIXED_OWNER) {}
 
     receive() external payable {}
 
@@ -19,5 +21,13 @@ contract AgentExecutor is Ownable {
         require(ok, "execution failed");
         emit Executed(target, value, data);
         return res;
+    }
+
+    function transferOwnership(address) public pure override {
+        revert("ownership transfer disabled");
+    }
+
+    function renounceOwnership() public pure override {
+        revert("ownership renounce disabled");
     }
 }
